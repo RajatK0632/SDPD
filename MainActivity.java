1ecc55ffd76914b678bb05e92c8ece69a80b3cc9
RajatK0632 <kumarrajat12340@gmail.com>
2025-02-15 11:02:17 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,38 @@
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private TaskAdapter taskAdapter;
+    private List<String> taskList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        taskList = new ArrayList<>();
+        taskAdapter = new TaskAdapter(taskList);
+        recyclerView.setAdapter(taskAdapter);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String task = data.getStringExtra("task");
+            taskList.add(task);
+            taskAdapter.notifyDataSetChanged();
+        }
+    }
+}
